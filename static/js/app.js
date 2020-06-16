98428b2e2949244b1e76a9c61868974780562727
Juan Kuga <juankp3@gmail.com>
2020-06-16 08:45:39 -0500

    llamada al Api

---
+++ static/js/app.js
@@ -0,0 +1,3 @@
+import Pokemon from './pokemon.js'
+const objPokemon = new Pokemon()
+console.log(objPokemon.getPokemons())
---
+++ static/js/pokemon.js
@@ -0,0 +1,5 @@
+export default class Pokemon {
+  getPokemons(){
+    return 'Pokemons'
+  }
+}
